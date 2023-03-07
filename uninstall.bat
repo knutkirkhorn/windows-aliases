ffb17eae642f712c33b95e2923354da5cd576f5c
Knut Kirkhorn <knutkirk@hotmail.com>
2023-03-08 00:03:21 +0100

    Add uninstall script and readme documentation

--- uninstall.bat
+++ uninstall.bat
@@ -4,12 +4,12 @@
 for /f "tokens=2,*" %%A in ('reg query "HKCU\Environment" /v PATH ^| find "PATH"') do set "currentPath=%%B"
 
 set aliasesDirectory=%CD%\aliases
-set newPath=%currentPath%;%aliasesDirectory%
+call set newPath=%%currentPath:%aliasesDirectory%=%%
 
-@REM Add aliases directory to PATH
+@REM Remove aliases directory to PATH
 REG ADD HKCU\Environment /v PATH /t REG_SZ /d "%newPath%" /f
 
 @REM Refresh the system settings by broadcasting the `WM_SETTINGCHANGE` message
 powershell -executionpolicy bypass -File ./refresh-system-settings.ps1
 
-echo Successfully installed aliases
+echo Successfully uninstalled aliases
